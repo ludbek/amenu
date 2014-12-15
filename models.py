1764571afce091c56e77012765283eefb33cd7da
ludbek <sth.srn@gmail.com>
2014-12-15 21:15:51 +0545

    extra_inactive defaults to 100 @GenericMenuModel

--- models.py
+++ models.py
@@ -16,12 +16,13 @@ class BreadcrumbModel(CMSPlugin):
     start_level = models.PositiveIntegerField(default = 0, help_text = "Level from where this breadcrumb will start. 0 = root level")
     template = models.CharField(max_length = 50, null = True, blank = True, help_text = "Path to the template for this menu. e.g. 'amenu/template_name.html'")
 
+
 class GenericMenuModel(CMSPlugin):
     """
     Model for generic menu. Makes use of 'show_menu' cms tag.
     """
     start_level = models.PositiveIntegerField(default = 0, help_text = "Level at which menu should start.")
     end_level = models.PositiveIntegerField(default = 100, help_text = "Level at which menu should end.")
-    extra_inactive = models.PositiveIntegerField(default = 0, help_text = "Level of navigation to be displayed if a node is not a direct ancestor or decendent of the current active node. 0 is root level.")
+    extra_inactive = models.PositiveIntegerField(default = 100, help_text = "Level of navigation to be displayed if a node is not a direct ancestor or decendent of the current active node. 0 is root level.")
     extra_active = models.PositiveIntegerField(default = 100, help_text = "Level of descendants of the currently active node to be displayed.")
     template = models.CharField(max_length = 50, null = True, blank = True, help_text = "Path to the template for this menu. e.g. 'amenu/template_name.html'")
