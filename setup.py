8a84be8962f1e0afefe8cb942fea53ca6cbac135
ludbek <sth.srn@gmail.com>
2014-12-18 17:07:18 +0545

    updated version to 1.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='amenu',
-    version='1.0.1',
+    version='1.0.2',
     author='ludbek',
     author_email='sth.srn@gmail.com',
     packages= ['amenu', 'amenu.migrations', 'amenu.templatetags'],
